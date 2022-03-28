4cfe7422c0847d4c927f68ba4568c1e0433d8828
Rebo Mark Agustines <rebo.agustines@ext.essilor.com>
2022-03-28 21:16:48 +0800

    create rg

--- deploy/dev/deploy.sh
+++ deploy/dev/deploy.sh
@@ -1,10 +1,11 @@
 rg_exists=$(az group exists -n $ENVIRONMENT-rg)
 echo "$rg_exists"
-if [ "$rg_exists" = "false" ];
+if [ "$rg_exists" = "true" ];
 then
     echo "$ENVIRONMENT-rg exists!"
 else
-    echo "$ENVIRONMENT-rg do not exists!"
+    echo "Creating Resource Group: $ENVIRONMENT-rg"
+    az group create -n $ENVIRONMENT-rg -l eastus -o table
+    az deployment group create -g $rg --template-file ./deploy/$ENVIRONMENT/azure-$ENVIRONMENT.bicep
 fi
 
-#az deployment group create -g $rg --template-file ./deploy/$ENVIRONMENT/azure-$ENVIRONMENT.bicep
