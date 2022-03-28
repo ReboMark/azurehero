e6872c14a7276dbca37e2aa4154bb752ac2f3069
Rebo Mark Agustines <rebo.agustines@ext.essilor.com>
2022-03-28 20:58:40 +0800

    fix deployment

--- deploy/dev/deploy.sh
+++ deploy/dev/deploy.sh
@@ -1,1 +1,10 @@
-az deployment group create -g $rg --template-file ./deploy/$ENVIRONMENT/azure-$ENVIRONMENT.bicep
+rg_exists=$(az group exists -n $ENVIRONMENT-rg)
+
+if [ "$rg_exists" = "true" ];
+then
+    echo "$ENVIRONMENT-rg exists!"
+else
+    echo "$ENVIRONMENT-rg do not exists!"
+fi
+
+#az deployment group create -g $rg --template-file ./deploy/$ENVIRONMENT/azure-$ENVIRONMENT.bicep
